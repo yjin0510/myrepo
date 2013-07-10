181605b99df886e7816fdbc38294be1306179cd0
Ubuntu <ubuntu@ip-172-31-29-107.us-west-2.compute.internal>
2013-07-10 19:27:40 +0000

    changed setup.py

--- setup.sh
+++ setup.sh
@@ -23,20 +23,20 @@ npm install -g jshint
 # See: http://nodejs.org/api/repl.html#repl_repl
 sudo apt-get install -y rlwrap
 
-# Install emacs24
-# https://launchpad.net/~cassou/+archive/emacs
-sudo apt-add-repository -y ppa:cassou/emacs
-sudo apt-get update
-sudo apt-get install -y emacs24 emacs24-el emacs24-common-non-dfsg
-
-# git pull and install dotfiles as well
-cd $HOME
-if [ -d ./dotfiles/ ]; then
-    mv dotfiles dotfiles.old
-fi
-if [ -d .emacs.d/ ]; then
-    mv .emacs.d .emacs.d~
-fi
+## Install emacs24
+## https://launchpad.net/~cassou/+archive/emacs
+#sudo apt-add-repository -y ppa:cassou/emacs
+#sudo apt-get update
+#sudo apt-get install -y emacs24 emacs24-el emacs24-common-non-dfsg
+#
+## git pull and install dotfiles as well
+#cd $HOME
+#if [ -d ./dotfiles/ ]; then
+#    mv dotfiles dotfiles.old
+#fi
+#if [ -d .emacs.d/ ]; then
+#    mv .emacs.d .emacs.d~
+#fi
 git clone https://github.com/startup-class/dotfiles.git
 ln -sb dotfiles/.screenrc .
 ln -sb dotfiles/.bash_profile .
